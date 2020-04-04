111e090801c54e4323cc4d5f512e55b33c405a55
Wenxuan Chen <wenx.chn@gmail.com>
2020-04-04 19:21:07 -0400

    Basic GoogleMaps API

---
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/FindNearestHospital.java
@@ -0,0 +1,39 @@
+package edu.upenn.cis350.advanceddirectives;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FindNearestHospital extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_nearest_hospital);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng current = new LatLng(39.9522, 75.1932);
+        map.addMarker(new MarkerOptions().position(current).title("current"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(current));
+
+    }
+}
