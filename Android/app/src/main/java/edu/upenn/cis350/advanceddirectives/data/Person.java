050463063929acf49f89c13648df6732b340caee
artisteford <45639289+artisteford@users.noreply.github.com>
2020-04-05 23:07:07 -0400

    constructor and getters and setters for person

--- Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/data/Person.java
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/data/Person.java
@@ -1,7 +1,7 @@
 package edu.upenn.cis350.advanceddirectives.data;
 
 public class Person {
-    static int currentID;
+    static int currentID = 0;
 
     private int ID;
     private Form form;
@@ -15,4 +15,79 @@ public class Person {
     private String email;
     private String phone;
     private String address;
+
+    public Person(String username, String password, String name, String email, String phone, String address) {
+        this.ID = currentID++;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public Form getForm() {
+        return this.form;
+    }
+
+    public String getPasscode() {
+        return this.passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
