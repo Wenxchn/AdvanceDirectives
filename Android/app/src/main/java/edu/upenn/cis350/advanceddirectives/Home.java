42bcfa2b0708f73708437a0024af4e4a1d942a79
artisteford <45639289+artisteford@users.noreply.github.com>
2020-04-06 02:55:34 -0400

    static person in Home

--- Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Home.java
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Home.java
@@ -9,16 +9,16 @@ import edu.upenn.cis350.advanceddirectives.data.Person;
 
 
 public class Home extends AppCompatActivity {
-    private Person currentUser;
+    static Person currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         String username = this.getIntent().getStringExtra("username");
-        this.currentUser = MainActivity.database.getPerson(username);
-        ((TextView) findViewById(R.id.name)).setText(
-                currentUser.getFirstName() + " " + currentUser.getLastName());
+        Home.currentUser = MainActivity.database.getPerson(username);
+        String name = Home.currentUser.getFirstName() + " " + currentUser.getLastName();
+        ((TextView) findViewById(R.id.name)).setText(name);
         ((TextView) findViewById(R.id.email)).setText(currentUser.getEmail());
         ((TextView) findViewById(R.id.phone)).setText(currentUser.getPhone());
         ((TextView) findViewById(R.id.address)).setText(currentUser.getAddress());
