e1f5e37d4adf2669210857dcdaffb1c9ee2d37e0
artisteford <45639289+artisteford@users.noreply.github.com>
2020-04-05 22:48:04 -0400

    new start screen to start various activities

---
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Login.java
@@ -0,0 +1,20 @@
+package edu.upenn.cis350.advanceddirectives;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginButtonClick(View view) {
+
+    }
+}
---
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Signup.java
@@ -0,0 +1,19 @@
+package edu.upenn.cis350.advanceddirectives;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void signupButtonClick(View view) {
+
+    }
+}
