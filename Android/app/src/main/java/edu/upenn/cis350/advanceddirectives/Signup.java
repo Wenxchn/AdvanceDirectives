b4cfb75db912d6a3db17835b698383358e342825
artisteford <45639289+artisteford@users.noreply.github.com>
2020-04-06 00:42:01 -0400

    signup stuff

--- Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Signup.java
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/Signup.java
@@ -1,9 +1,15 @@
 package edu.upenn.cis350.advanceddirectives;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import edu.upenn.cis350.advanceddirectives.data.Database;
+import edu.upenn.cis350.advanceddirectives.data.Person;
 
 public class Signup extends AppCompatActivity {
 
@@ -13,7 +19,25 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
     }
 
-    public void signupButtonClick(View view) {
-
+    public void signupSubmitButtonClick(View view) {
+        Database db = MainActivity.database;
+        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passowrd)).getText().toString();
+        String name = ((EditText) findViewById(R.id.name)).getText().toString();
+        String email = ((EditText) findViewById(R.id.email)).getText().toString();
+        String phone = ((EditText) findViewById(R.id.phone)).getText().toString();
+        String address = ((EditText) findViewById(R.id.address)).getText().toString();
+        if (db.isAvailable(username)) {
+            Person currentUser = new Person(db.getAndIncrementCurrID(), username, password,
+                    name, email, phone, address);
+            db.addPerson(currentUser);
+            Toast.makeText(this, "Account created, you can now log in!",
+                    Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, Login.class));
+        } else {
+            String message = "We're sorry, this username is already in use, " +
+                    "please choose another username!";
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
     }
 }
