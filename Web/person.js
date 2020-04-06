e8db4f48a5860d3225b4940653f4b8efb8fffddf
Zhiyan Qiu <zhiyan.qiu@gmail.com>
2020-04-05 20:24:18 -0400

    search for people by first/last name

---
+++ Web/person.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    birthdate: Date,
+    address: String,
+    email: String
+});
+
+var mongoURI = "mongodb+srv://zqiu:12345@cluster0-bmaew.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'CIS350'
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("MongoDB connected");
+});
+
+var People = mongoose.model('Person', personSchema, 'persons');
+module.exports = People;
