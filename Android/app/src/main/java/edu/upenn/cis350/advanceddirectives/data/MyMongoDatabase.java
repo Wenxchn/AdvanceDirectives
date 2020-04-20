2a3a540165f51010c5b375325b78b2ac99d8394a
artisteford <45639289+artisteford@users.noreply.github.com>
2020-04-20 06:05:03 -0400

    mongo using node first attempt

--- Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/data/MyMongoDatabase.java
+++ Android/app/src/main/java/edu/upenn/cis350/advanceddirectives/data/MyMongoDatabase.java
@@ -2,135 +2,121 @@ package edu.upenn.cis350.advanceddirectives.data;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-
-import org.bson.Document;
-
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class MyMongoDatabase extends Database {
 
     WebStuff web;
 
     private class WebStuff extends AsyncTask<Object, Void, Object> {
-        ConnectionString s = new ConnectionString(
-                "mongodb+srv://zqiu:12345@cluster0-bmaew.mongodb.net/" +
-                        "CIS350?retryWrites=true&w=majority");
 
-        MongoClient mongoClient = MongoClients.create(s);
-
-//                "mongodb://[username:password@]host1[:port1][,host2[:port2]
-//                ,...[,hostN[:portN]]][/[database][?options]]"
-
-        MongoDatabase db = mongoClient.getDatabase("CIS350");
-        MongoCollection<Document> collection = db.getCollection("persons");
-
-        private Person getPersonFromDB(Document dbPerson) {
-            if (dbPerson == null) { return null; }
-            String username = (String) dbPerson.get("username");
-            String password = (String) dbPerson.get("password");
-            String firstName = (String) dbPerson.get("firstName");
-            String lastName = (String) dbPerson.get("lastName");
-            String email = (String) dbPerson.get("email");
-            String phone = (String) dbPerson.get("phone");
-            String address = (String) dbPerson.get("address");
-            String birthday = (String) dbPerson.get("birthday");
-            Person person = new Person(username, password,
-                    firstName, lastName, email, phone, address, birthday);
-            person.setForm((String[]) dbPerson.get("form"));
-            person.setMoodCalendar((String) dbPerson.get("moodCalendar"));
-            return person;
+        private Person getPersonFromJSON(JSONObject dbPerson) {
+            if (dbPerson.equals(new JSONObject())) { return null; }
+            try {
+                String username = (String) dbPerson.get("username");
+                String password = (String) dbPerson.get("password");
+                String firstName = (String) dbPerson.get("firstName");
+                String lastName = (String) dbPerson.get("lastName");
+                String email = (String) dbPerson.get("email");
+                String phone = (String) dbPerson.get("phone");
+                String address = (String) dbPerson.get("address");
+                String birthday = (String) dbPerson.get("birthday");
+                Person person = new Person(username, password,
+                        firstName, lastName, email, phone, address, birthday);
+                person.setForm((String[]) dbPerson.get("form"));
+                person.setMoodCalendar((String) dbPerson.get("moodCalendar"));
+                return person;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
 
-        private Document getDocumentFromPerson(Person person) {
-            Document dbPerson = new Document();
-            dbPerson.put("username", person.getUsername());
-            dbPerson.put("password", person.getPassword());
-            dbPerson.put("firstName", person.getFirstName());
-            dbPerson.put("lastName", person.getLastName());
-            dbPerson.put("email", person.getEmail());
-            dbPerson.put("phone", person.getPhone());
-            dbPerson.put("address", person.getAddress());
-            dbPerson.put("birthday", person.getBirthday());
-            dbPerson.put("moodCalendar", person.getMoodCalendar().toString());
-            dbPerson.put("form", person.getForm().getAnswers());
-            return dbPerson;
+        private JSONObject getJSONFromPerson(Person person) {
+            JSONObject dbPerson = new JSONObject();
+            try {
+                dbPerson.put("username", person.getUsername());
+                dbPerson.put("password", person.getPassword());
+                dbPerson.put("firstName", person.getFirstName());
+                dbPerson.put("lastName", person.getLastName());
+                dbPerson.put("email", person.getEmail());
+                dbPerson.put("phone", person.getPhone());
+                dbPerson.put("address", person.getAddress());
+                dbPerson.put("birthday", person.getBirthday());
+                dbPerson.put("moodCalendar", person.getMoodCalendar().toString());
+                dbPerson.put("form", person.getForm().getAnswers());
+                return dbPerson;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private Object getWebStuffs(String url) {
+            try {
+                URL u = new URL(url);
+                HttpURLConnection c = (HttpURLConnection) u.openConnection();
+                c.setRequestMethod("GET");
+                c.connect();
+                Scanner in = new Scanner(u.openStream());
+                StringBuilder response = new StringBuilder();
+                while (in.hasNext()) {
+                    response.append(in.nextLine());
+                }
+                in.close();
+                c.disconnect();
+                return new JSONObject(response.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
 
         @Override
         protected Object doInBackground(Object... objects) {
             String action = (String) objects[0];
-            switch (action) {
-                case "get": {
-                    Document dbPerson = collection.find(Filters.eq("username",
-                            (String) objects[1])).first();
-                    return getPersonFromDB(dbPerson);
-                }
-                case "set": {
-                    Person person = (Person) objects[1];
-                    Person previous = getPersonFromDB(collection.findOneAndDelete(
-                            Filters.eq("username", person.getUsername())));
-                    Document dbPerson = getDocumentFromPerson(person);
-                    collection.insertOne(dbPerson);
-                    return previous;
-                }
-                case "remove": {
-                    String username = (String) objects[1];
-                    return getPersonFromDB(collection.findOneAndDelete(
-                            Filters.eq("username", username)));
-                }
-                case "all": {
-                    Collection<Person> people = new LinkedList<>();
-                    for (Document dbPerson: collection.find()) {
-                        people.add(getPersonFromDB(dbPerson));
+            String urlStart = "http://localhost:5500/";
+            try {
+                switch (action) {
+                    case "get": {
+                        String url = urlStart + "get" + "?username=" + objects[1];
+                        return getPersonFromJSON((JSONObject) getWebStuffs(url));
                     }
-                    return people;
-                }
-                case "available": {
-                    String username = (String) objects[1];
-                    for (Document dbPerson: collection.find()) {
-                        if (((String) dbPerson.get("username")).equals(username)) {
-                            return false;
+                    case "set": {
+                        Person person = (Person) objects[1];
+                        String url = urlStart + "set" + "?person=" + getJSONFromPerson(person);
+                        return getPersonFromJSON((JSONObject) getWebStuffs(url));
+                    }
+                    case "remove": {
+                        String url = urlStart + "remove" + "?username=" + objects[1];
+                        return getPersonFromJSON((JSONObject) getWebStuffs(url));
+                    }
+                    case "all": {
+                        Collection<Person> people = new LinkedList<>();
+                        String url = urlStart + "all";
+                        JSONArray arr = (JSONArray) getWebStuffs(url);
+                        for (int i = 0; i < arr.length(); i++) {
+                            JSONObject dbPerson = (JSONObject) arr.get(i);
+                            people.add(getPersonFromJSON(dbPerson));
                         }
+                        return people;
                     }
-                    return true;
+                    case "available": {
+                        String url = urlStart + "remove" + "?username=" + objects[1];
+                        return getWebStuffs(url);
+                    }
                 }
-                case "iterator": {
-                    return new PersonIterator();
-                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
             return null;
-        }
-        private class PersonIterator implements Iterator<Person> {
-            MongoCursor<Document> weirdIterator;
-
-            public PersonIterator() {
-                weirdIterator = collection.find().iterator();
-            }
-
-            @Override
-            public boolean hasNext() {
-                return weirdIterator.hasNext();
-            }
-
-            @Override
-            public Person next() {
-                if (hasNext()) {
-                    return getPersonFromDB(weirdIterator.next());
-                } else {
-                    throw new NoSuchElementException();
-                }
-            }
         }
     }
 
@@ -184,18 +170,6 @@ public class MyMongoDatabase extends Database {
             throw new RuntimeException();
         }
         return people;
-    }
-
-    @Override
-    public Iterator<Person> getPeopleIterator() {
-        Iterator<Person> peopleIterator;
-        try {
-            web.execute("iterator");
-            peopleIterator = (Iterator<Person>) web.get();
-        } catch (Exception e) {
-            throw new RuntimeException();
-        }
-        return peopleIterator;
     }
 
     @Override
